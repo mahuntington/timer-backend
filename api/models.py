8875353a2413a05a3d1c8d079801a8408b16ba9a
Matt Huntington <matt.huntington@gmail.com>
2021-04-02 10:40:17 -0400

    creating session model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Session(models.Model):
+    seconds = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
