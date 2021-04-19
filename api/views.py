00c1ba0b497b21244fa33893c36699806d1cd97a
Matt Huntington <matt.huntington@gmail.com>
2021-04-19 15:22:34 -0400

    order by -id

--- api/views.py
+++ api/views.py
@@ -3,9 +3,9 @@ from .serializers import SessionSerializer
 from .models import Session
 
 class SessionList(generics.ListCreateAPIView):
-    queryset = Session.objects.all() # tell django how to retrieve all objects from the DB
+    queryset = Session.objects.all().order_by('-id') # tell django how to retrieve all objects from the DB
     serializer_class = SessionSerializer # tell django what serializer to use
 
 class SessionDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Session.objects.all()
+    queryset = Session.objects.all().order_by('-id')
     serializer_class = SessionSerializer
