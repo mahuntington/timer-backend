e2421392a2912960f0a52c1ec4e9fddb3a3e973c
Matt Huntington <matt.huntington@gmail.com>
2021-09-26 13:03:19 -0400

    adding local time

--- api/models.py
+++ api/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from datetime import timedelta 
 
-# Create your models here.
 class Session(models.Model):
     seconds = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def local_time(self):
+        local_date = self.created_at - timedelta(hours=4)
+        return local_date.strftime('%h %d, %Y at %I:%M:%S %p')
 
