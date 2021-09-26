5da587e6b240c1363ca69c5370a27c873c5bc375
Matt Huntington <matt.huntington@gmail.com>
2021-09-26 13:09:55 -0400

    changing field to local date

--- api/models.py
+++ api/models.py
@@ -5,7 +5,7 @@ class Session(models.Model):
     seconds = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def local_time(self):
+    def local_date(self):
         local_date = self.created_at - timedelta(hours=4)
         return local_date.strftime('%h %d, %Y at %I:%M:%S %p')
 
